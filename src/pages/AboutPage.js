9315107a4f53766615f0e8f0051716c130efe5fd
Headmaster <aliubashirtaiye2002@gmail.com>
2022-11-14 07:29:29 +0100

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,8 +3,15 @@ import styled from 'styled-components'
 import { Products12 } from '../utils/constants'
 import star from '../images/start.png'
 import { GiSettingsKnobs } from 'react-icons/gi'
+import meta from '../images/metamask.png'
+import trust from '../images/trust.png'
+import { IoIosArrowForward } from 'react-icons/io'
+import { VscClose } from 'react-icons/vsc'
+import { useGlobalContext } from '../context'
 
 const AboutPage = () => {
+  const { open, setOpen } = useGlobalContext()
+
   return (
     <Wrapper>
       <div className='rest'>
@@ -21,6 +28,41 @@ const AboutPage = () => {
           <GiSettingsKnobs />
         </div>
       </div>
+      {open && (
+        <div className='wallet'>
+          <div className='overlay'></div>
+
+          <div className='pop-up'>
+            <div className='head-pop'>
+              <p>connect wallet</p>
+              <VscClose
+                className='close'
+                onClick={() => {
+                  setOpen(false)
+                }}
+              />
+            </div>
+            <div className='pop-content'>
+              <p id='choose'>Choose your preferred wallet:</p>
+              <div className='wall'>
+                <div className='pop-img'>
+                  <img src={meta} alt='' className='arrow' />
+                  <p>Metamask</p>
+                </div>
+                <IoIosArrowForward />
+              </div>
+              <div className='wall'>
+                <div className='pop-img'>
+                  <img src={trust} alt='' className='arrow' />
+                  <p>WalletConnect</p>
+                </div>
+                <IoIosArrowForward />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className='proj'>
         <div className='projects'>
           {Products12.map((item) => {
@@ -91,6 +133,68 @@ const Wrapper = styled.section`
       font-size: 1.2rem;
     }
   }
+  .pop-up {
+    width: 40%;
+    position: fixed;
+    left: 35%;
+    top: 40%;
+    background-color: white;
+    border-radius: 10px;
+    border: none;
+    z-index: 99;
+    p {
+      margin-bottom: 0;
+      color: black;
+    }
+  }
+  .head-pop {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 2rem;
+    align-items: center;
+    border-bottom: 1px dashed black;
+  }
+  .close {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+  .wall {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    border: 1px dashed black;
+    border-radius: 5px;
+    justify-content: space-between;
+    margin-top: 1rem;
+  }
+  .overlay {
+    position: fixed;
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+  .pop-img {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+  .pop-content {
+    padding: 1rem 2rem;
+    font-size: 1.5rem;
+  }
+  .arrow {
+    font-size: 1.2rem;
+  }
+  #choose {
+    font-size: 1rem;
+    // padding-bottom:1rem;
+  }
   .infos img {
     padding: 0 0.5rem;
   }
@@ -141,6 +245,11 @@ const Wrapper = styled.section`
     }
     .rest p {
       font-size: 0.8rem;
+    }
+    .pop-up {
+      width: 90%;
+      left: 5%;
+      top: 41%;
     }
   }
   @media (max-width: 550px) {
